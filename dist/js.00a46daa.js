dc4b6e55b6af99ea91c092531dfce498f3a39c40
Jagb <Jagb.dot@gmail.com>
2020-12-08 00:38:04 -0300

    details 2

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -6404,7 +6404,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59897" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57819" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
